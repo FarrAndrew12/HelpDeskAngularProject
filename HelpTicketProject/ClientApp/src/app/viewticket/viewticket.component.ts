fe3ffd5ef4de9c0b269517cbc6cbdf60da18f241
FarrAndrew12 <101841142+FarrAndrew12@users.noreply.github.com>
2022-06-13 15:29:07 -0400

    final commit

--- HelpTicketProject/ClientApp/src/app/viewticket/viewticket.component.ts
+++ HelpTicketProject/ClientApp/src/app/viewticket/viewticket.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TicketServiceService } from '../ticket-service.service';
 import { Ticket } from '../helpticket';
-
+import { UserInput } from '../helpticket';
 @Component({
   selector: 'app-viewticket',
   templateUrl: './viewticket.component.html',
@@ -9,18 +9,19 @@ import { Ticket } from '../helpticket';
   providers:[]
 })
 export class ViewticketComponent implements OnInit {
-
   t: Ticket = new Ticket (0, "",0,"",0,false);
+  User: UserInput[] = [];
   
-  constructor(private helpticket :TicketServiceService) {
-    
-   }
-
-  ngOnInit(): void {
+  constructor(public helpticket :TicketServiceService) {
     this.t=this.helpticket.ticketmodel;
     console.log(this.helpticket.ticketmodel);
+    this.helpticket.getUserInput().subscribe(
+      (result) => {this.User = result;
+        console.log(result) }
+    );
+   }
+  ngOnInit(): void {
+    
   }
-
   
-
 }
